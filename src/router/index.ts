a9fa7d53d34c10d4f3ae4e2102172a062ca46637
Svilen Popov <svilen.popov@mentormate.com>
2022-05-21 16:22:00 +0300

    configure new routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,18 +1,33 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      redirect: {
+        name: 'games',
+      },
     },
     {
-      path: '/new-game',
+      path: '/games',
+      name: 'games',
+      component: () => import('../views/Games.vue'),
+    },
+    {
+      path: '/games/new',
       name: 'new-game',
       component: () => import('../views/NewGame.vue'),
+    },
+    {
+      path: '/games/:id',
+      name: 'game-details',
+      component: () => import('../views/GameDetails.vue'),
+    },
+    {
+      path: '/games/:id/add-round',
+      name: 'game-round',
+      component: () => import('../views/GameRound.vue'),
     },
   ],
 });
---
+++ src/views/GameRound.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>Game Round - To be implemented</div>
+</template>
